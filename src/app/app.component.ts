d57d76e66ca21308ae47e86413360c38b06b25c4
dteran24 <dteran21@yahoo.com>
2024-05-09 18:32:46 -0500

    creating class and card compnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Question } from './models/question';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'interview-helper';
+  questions = [
+    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
+    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
+    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
+    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
+  ];
 }
---
+++ src/app/models/question.ts
@@ -0,0 +1,9 @@
+export class Question {
+  constructor(
+    public id: number,
+    public question: string,
+    public answer: string,
+    public tag: string,
+    public difficulty: number
+  ) {}
+}
