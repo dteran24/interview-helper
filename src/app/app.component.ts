2cd68b734d929148dd5d921890a413d684ce7099
dteran24 <dteran21@yahoo.com>
2024-05-09 20:02:26 -0500

    setting up format for cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Question } from './models/question';
-import { CardComponent } from "./components/card/card.component";
+import { CardComponent } from './components/card/card.component';
+import { NgClass } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.sass',
-    imports: [RouterOutlet, CardComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass',
+  imports: [RouterOutlet, CardComponent, NgClass],
 })
 export class AppComponent {
+  isSelected = true;
   questions = [
-    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
-    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
-    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
-    new Question(1, "What is the State Capital?", "Austin", "Geographic", 1),
+    new Question(1, 'What is the State Capital?', 'Austin', 'Geographic', 1),
+    new Question(1, 'What is the State Capital?', 'Austin', 'Geographic', 1),
+    new Question(1, 'What is the State Capital?', 'Austin', 'Geographic', 1),
+    new Question(1, 'What is the State Capital?', 'Austin', 'Geographic', 1),
   ];
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Question } from '../../models/question';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'question-card',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.sass'
+  styleUrl: './card.component.sass',
 })
 export class CardComponent {
+  @Input() isSelected!: boolean
   @Input() question: Question = {} as Question;
 }
