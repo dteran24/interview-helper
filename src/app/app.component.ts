fd4e8abf49ee83ed930e2467a71a4c0638ec3227
dteran24 <dteran21@yahoo.com>
2024-05-09 19:23:56 -0500

    display cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Question } from './models/question';
+import { CardComponent } from "./components/card/card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.sass',
+    imports: [RouterOutlet, CardComponent]
 })
 export class AppComponent {
   questions = [
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Question } from '../../models/question';
 
 @Component({
-  selector: 'app-card',
+  selector: 'question-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
   styleUrl: './card.component.sass'
 })
 export class CardComponent {
-
+  @Input() question: Question = {} as Question;
 }
